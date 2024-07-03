47cf27d4334ce26ac2d9d641a635c8101fabb650
Chanhui-lee <127923834+leechanang@users.noreply.github.com>
2024-07-03 16:03:01 +0900

    Rename 2주차_1_34page.cpp to 2주차_1_34page.cpp

--- 영상처리/2주차_1_34page.cpp
+++ 영상처리/2주차_1_34page.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
 	Scalar orange(0, 165, 255), blue(255, 0, 0), magenta(255, 0, 255);
-	Mat image(300, 500, CV_8UC3, Scalar(255, 255, 255)); //Èò»ö ÄÃ·¯ ¿µ»ó
+	Mat image(300, 500, CV_8UC3, Scalar(255, 255, 255)); //í°ìƒ‰ ì»¬ëŸ¬ ì˜ìƒ
 	
-	Point center = (Point)image.size() / 2; // ¿µ»ó Áß½ÉÁÂÇ¥
+	Point center = (Point)image.size() / 2; // ì˜ìƒ ì¤‘ì‹¬ì¢Œí‘œ
 	Point pt1(70, 50), pt2(350, 220);
 
-	circle(image, center, 100, blue); //¿ø±×¸®±â
+	circle(image, center, 100, blue); //ì›ê·¸ë¦¬ê¸°
 	circle(image, pt1, 80, orange, 2); 
-	circle(image, pt2, 60, magenta, -1); //¿ø ³»ºÎ Ã¤¿ò
+	circle(image, pt2, 60, magenta, -1); //ì› ë‚´ë¶€ ì±„ì›€
 
 	int font = FONT_HERSHEY_COMPLEX;
 	putText(image, "center_blue", center, font, 1.2, blue);
@@ -19,7 +19,7 @@ int main()
 	putText(image, "pt2_magenta", pt2 + Point(2, 2), font, 0.5, Scalar(0,0,0), 2);
 	putText(image, "pt2_magenta", pt2, font, 0.5, magenta, 1);
 
-	imshow("¿ø±×¸®±â", image);
+	imshow("ì›ê·¸ë¦¬ê¸°", image);
 	waitKeyEx(0);
 	return 0;
 }
