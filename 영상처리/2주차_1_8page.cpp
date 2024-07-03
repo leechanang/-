e03746d291773d454689adad4ed4db9755353800
Chanhui-lee <127923834+leechanang@users.noreply.github.com>
2024-07-03 16:03:31 +0900

    Rename 2주차_1_8page.cpp to 2주차_1_8page.cpp

--- 영상처리/2주차_1_8page.cpp
+++ 영상처리/2주차_1_8page.cpp
@@ -2,24 +2,24 @@
 
 int main()
 {
-	//point_ °´Ã¼ ¼±¾ğ ¹æ½Ä
+	//point_ ê°ì²´ ì„ ì–¸ ë°©ì‹
 	cv::Point_<int> pt1(100, 200);
 	cv::Point_<float> pt2(92.3f, 125.23f);
 	cv::Point_<double> pt3(100.2, 300.9);
 
-	//point_ °´Ã¼ °£°á ¼±¾ğ ¹æ½Ä
+	//point_ ê°ì²´ ê°„ê²° ì„ ì–¸ ë°©ì‹
 	cv::Point2i pt4(120, 69);
 	cv::Point2f pt5(0.3f, 0.f), pt6(0.f, 0.4f);
 	cv::Point2d pt7(0.25, 0.6);
 
-	//point_ °´Ã¼ ¿¬»ê
-	cv::Point pt8 = pt1 + (cv::Point) pt2; //ÀÚ·áÇüÀÌ ´Ù¸¥ point °´Ã¼ µ¡¼À
+	//point_ ê°ì²´ ì—°ì‚°
+	cv::Point pt8 = pt1 + (cv::Point) pt2; //ìë£Œí˜•ì´ ë‹¤ë¥¸ point ê°ì²´ ë§ì…ˆ
 	cv::Point2f pt9 = pt6 * 3.14f;  // point 
 	cv::Point2d pt10 = (pt3 + (cv::Point2d) pt6) * 10;
 
 	std::cout << "pt8 =" << pt8.x << " , " << pt8.y << std::endl;
 	std::cout << "[pt9] = " << pt9 << std::endl;
 	std::cout << (pt2 == pt6) << std::endl;
-	std::cout << "pt7°ú pt8ÀÇ ³»Àû : " << pt7.dot(pt8) << std::endl;
+	std::cout << "pt7ê³¼ pt8ì˜ ë‚´ì  : " << pt7.dot(pt8) << std::endl;
 	return 0;
 }
